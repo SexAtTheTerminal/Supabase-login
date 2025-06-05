46c7022d189178e79d744a6667d42e5d9126e735
Gabriel Cuba <gabrielcuba2017@gmail.com>
2025-06-05 04:47:53 -0500

    CashierMain1.4

--- src/app/modules/pedidos/registrar-pedidos/registrar-pedidos.component.ts
+++ src/app/modules/pedidos/registrar-pedidos/registrar-pedidos.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { SidebarCasherComponent } from "../../../sidebar/features/sidebar-casher/sidebar-casher.component";
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-pedidos',
-  imports: [SidebarCasherComponent],
+  imports: [SidebarCasherComponent, CommonModule, RouterLink],
   templateUrl: './registrar-pedidos.component.html',
-  styleUrl: './registrar-pedidos.component.scss'
+  styleUrl: './registrar-pedidos.component.scss',
 })
 export class RegistrarPedidosComponent {
 
+  sidebarCollapsed = false;
+
+  onSidebarToggle(state: boolean): void {
+    this.sidebarCollapsed = state;
+  }
 }
