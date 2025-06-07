4a28c3355ed77a7628c8160c4f03c9425e8af169
PieroHua <piale820@gmail.com>
2025-06-06 19:12:08 -0500

    Avance Piero H

--- src/app/modules/pagos/registrar-cobro/registrar-cobro.component.ts
+++ src/app/modules/pagos/registrar-cobro/registrar-cobro.component.ts
@@ -1,12 +1,24 @@
+import { signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SidebarCasherComponent } from "../../../sidebar/features/sidebar-casher/sidebar-casher.component";
 
+
 @Component({
   selector: 'app-registrar-cobro',
-  imports: [SidebarCasherComponent],
+  imports: [SidebarCasherComponent, CommonModule, RouterLink],
   templateUrl: './registrar-cobro.component.html',
   styleUrl: './registrar-cobro.component.scss'
 })
 export class RegistrarCobroComponent {
+
+  sidebarCollapsed = false;
+
+  onSidebarToggle(state: boolean): void{
+    this.sidebarCollapsed = state;
+  }
+
+
 
 }
