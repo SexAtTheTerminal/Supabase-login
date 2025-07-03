ac5946639013b09add11c7d484660d80014f3083
PieroHua <piale820@gmail.com>
2025-07-03 11:10:55 -0500

    Insersion de cobro clientes y pagos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< HEAD
-import { provideHttpClient } from '@angular/common/http'; // <-- Importa esto
 
-import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-=======
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
@@ -20,5 +11,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
   ],
->>>>>>> main
+
 };
